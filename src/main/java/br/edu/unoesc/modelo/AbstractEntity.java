09a9ee7c82461190a83d41bb4ac21edcb894c0a1
Marco Tortelli <marcopazinitortelli@gmail.com>
2024-11-22 10:34:18 -0300

    Pequenos Ajustes

--- src/main/java/br/edu/unoesc/modelo/AbstractEntity.java
+++ src/main/java/br/edu/unoesc/modelo/AbstractEntity.java
@@ -3,7 +3,10 @@ package br.edu.unoesc.modelo;
 import java.io.Serializable;
 import java.util.Objects;
 
-import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 
 
 @SuppressWarnings("serial")
